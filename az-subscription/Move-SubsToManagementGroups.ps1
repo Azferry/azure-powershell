3987eac8fc02e7ccf14a50e5241322814b0e6b07
Ryan Ferry <ryanferry@microsoft.com>
2022-02-19 12:41:20 -0500

    MG Group Move

--- az-subscription/Move-SubsToManagementGroups.ps1
+++ az-subscription/Move-SubsToManagementGroups.ps1
@@ -7,7 +7,7 @@
 .PARAMETER DestinationManagementGroupName
   Name of the management group the subscriptions will be moved to 
 .PARAMETER CSVPath
-  Path to the CSV input file
+  Path to the CSV input file. CSV can be generatred with Pull-ReportOnMsdnSubs.ps1
 .PARAMETER WhatIf
   True or False to allow the user to see what changes will be made in the environment 
 .NOTES
@@ -16,9 +16,22 @@
 .NOTES
   Version:        1.0
   Creation Date:  2/17/22
-  Purpose/Change: Initial script development
+  Change: V1.0 - Initial script development
+.NOTES
+  LEGAL DISCLAIMER:
+  This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment. 
+  THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+  INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+  We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that You agree:
+  (i) to not use Our name, logo, or trademarks to market Your software product in which the Sample Code is embedded;
+  (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; and
+  (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys' fees, that arise or result from the use or distribution of the Sample Code.
+  This posting is provided "AS IS" with no warranties, and confers no rights.
 .EXAMPLE
+  Run script 
   .\Move-SubsToManagementGroups.ps1 -DestinationManagementGroupName "ntc-lz-sbx" -CSVPath "..\AzSubscriptions.csv" 
+.EXAMPLE
+  Run script with What if allows you to check what will happen before running
   .\Move-SubsToManagementGroups.ps1 -DestinationManagementGroupName "ntc-lz-sbx" -CSVPath "..\AzSubscriptions.csv"  -WhatIf $true
 #>
 
@@ -29,7 +42,7 @@ Param (
 [Parameter(Mandatory=$false)][bool]$WhatIf = $false
 )
 
-# Connect-AzAccount
+Connect-AzAccount
 
 try {
   $SubList = Import-CSV -Path ..\AzSubscriptions.csv
@@ -39,17 +52,18 @@ try {
 
   foreach ($sub in $SubList){
     if($WhatIf){
-      New-AzManagementGroupSubscription -GroupName $DestinationManagementGroupName -SubscriptionId $sub.SubscriptionId -WhatIf
+      New-AzManagementGroupSubscription -GroupId $DestinationManagementGroupName -SubscriptionId $sub.SubscriptionId -WhatIf
+      Write-Host ("WhatIf - Move Sub: " + $sub.Name + " to Management group:  $DestinationManagementGroupName") -ForegroundColor Yellow
     }
     else {
-      New-AzManagementGroupSubscription -GroupName $DestinationManagementGroupName -SubscriptionId $sub.SubscriptionId 
+      New-AzManagementGroupSubscription -GroupId $DestinationManagementGroupName -SubscriptionId $sub.SubscriptionId 
       Write-Host ("Move Sub: " + $sub.Name + " to Management group:  $DestinationManagementGroupName") -ForegroundColor Green
     }
   }
  
 }
 catch {
-  Write-Host "Error" -ForegroundColor Red
+  Write-Host "Error running the script, Check premissions and input" -ForegroundColor Red
 }
 finally {
   Write-Host "Script complete" -ForegroundColor Green
