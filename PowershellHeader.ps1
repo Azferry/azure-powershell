91cfc20b2a838509af2ae52441827ee9be5752a1
Ryan Ferry <ryanferry@microsoft.com>
2022-02-23 12:55:37 -0500

    New EA Subscription

--- PowershellHeader.ps1
+++ PowershellHeader.ps1
@@ -24,6 +24,10 @@
   (ii) to include a valid copyright notice on Your software product in which the Sample Code is embedded; and
   (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys' fees, that arise or result from the use or distribution of the Sample Code.
   This posting is provided "AS IS" with no warranties, and confers no rights.
+.COMPONENT 
+  Requires Module ActiveDirectory
+.LINK 
+  https://github.com/Azferry/azure-powershell
 .EXAMPLE
   <EXAMPLE>
 #>
--- az-subscription/New-AzureEaSubscription.ps1
+++ az-subscription/New-AzureEaSubscription.ps1
@@ -1,20 +1,71 @@
 #requires -version 2
 <#
 .SYNOPSIS
-  <SYNOPIS>
+  Create a new subscription with in an EA Agreement
 .DESCRIPTION
-  <DESCRIPTION>
-.PARAMETER X
-  <PRAM>
-.PARAMETER Y
-  <PRAM>
-.INPUTS
-  <TEMPLATE>
+  Creating new subscriptions from within an EA Agreement, and moving them to the 
+  management group after creation.
+.PARAMETER BillingScope
+  Scope of billing within the EA portal
+  Ex: "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321"
+.PARAMETER WorkloadType
+  Type of subscription (DevTest or Production)
+.PARAMETER SubAlias
+  Subscription  Alias for the new subscription (can be the same as name)
+.PARAMETER SubscriptionName
+  Name of the new subscription 
+.PARAMETER DestinationManagementGroupId
+  The ID Of the management group the subscription should be placed in
+.COMPONENT 
+  Requires Module Az.Subscription
+.LINK 
+  https://github.com/Azferry/azure-powershell
 .NOTES
   Version:        1.0
-  Creation Date:  2/17/22
+  Creation Date:  2/18/22
   Change: V1.0 - Initial script development
 .EXAMPLE
-  <EXAMPLE>
+  Create a Production Subscription 
+  .\New-AzureEaSubscription.ps1 -DestinationManagementGroupId "ntc-lz-sbx" -SubscriptionName "Name" -SubAlias "Name" -BillingScope "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321"
+.EXAMPLE
+  Create a DevTest Subscription 
+  .\New-AzureEaSubscription.ps1 -DestinationManagementGroupId "ntc-lz-sbx" -SubscriptionName "Name" -SubAlias "Name" -BillingScope "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321" -WorkloadType "DevTest"
 #>
 
+
+Param (
+  [Parameter(Mandatory=$true)][string]$SubAlias,
+  [Parameter(Mandatory=$true)][string]$SubscriptionName,
+  [Parameter(Mandatory=$true)][string]$DestinationManagementGroupId,
+  [Parameter(Mandatory=$true)][string]$BillingScope,
+  [Parameter(Mandatory=$false)][string]$WorkloadType = "Production"
+)
+
+Connect-AzAccount 
+
+$WorkloadType = "DevTest"
+$BillingScope = ""
+$SubAlias = "NewAlias2"
+$SubscriptionName = "Subscription2"
+$DestinationManagementGroupId = ""
+
+
+if (Get-Module -ListAvailable -Name "Az.Subscription") {
+  Write-Host "Module Already installed on the system"
+}
+else {
+  Write-Host "Module does not exist - installing module"
+  Install-Module Az.Subscription
+}
+Import-Module Az.Subscription
+
+
+$EaSub = New-AzSubscriptionAlias -AliasName $SubAlias `
+      -SubscriptionName $SubscriptionName `
+      -BillingScope $BillingScope `
+      -Workload $WorkloadType
+
+Write-Host "New Subscription Created" -ForegroundColor Green
+
+New-AzManagementGroupSubscription -GroupId $DestinationManagementGroupId -SubscriptionId $EaSub.Id  
+Write-Host ("Move Sub: " + $EaSub.Id  + " to Management group:  $DestinationManagementGroupId") -ForegroundColor Green
