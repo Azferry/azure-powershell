844faa397d265296d6f76819628f565d5571b452
Ryan Ferry <ryanferry@microsoft.com>
2022-03-17 21:01:39 -0400

    Role And User

--- az-subscription/New-AzureEaSubscription.ps1
+++ az-subscription/New-AzureEaSubscription.ps1
@@ -16,15 +16,20 @@
   Name of the new subscription 
 .PARAMETER DestinationManagementGroupId
   The ID Of the management group the subscription should be placed in
+.PARAMETER UserUPN 
+  Optional - The full user principle name of the aad user for the role assignment to subscription
+.PARAMETER RoleNmae
+  Optional - name of the role to give to the user defined in the UPN
 .COMPONENT 
   Requires Module Az.Subscription
 .LINK 
   https://github.com/Azferry/azure-powershell
 .NOTES
-  Version:        1.1
+  Version:        1.2
   Creation Date:  2/18/22
   Change: V1.0 - Initial script development
           V1.1 - Mv subscription to mg group
+          v1.2 - Add role assignments and user
 .NOTES
   There's a limit of 5000 subscriptions per enrollment account. After that, more subscriptions for the account can only be
   created in the Azure portal. To create more subscriptions through the API, create another enrollment account. Canceled,
@@ -35,6 +40,12 @@
 .EXAMPLE
   Create a DevTest Subscription 
   .\New-AzureEaSubscription.ps1 -DestinationManagementGroupId "ntc-lz-sbx" -SubscriptionName "Name" -SubAlias "Name" -BillingScope "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321" -WorkloadType "DevTest"
+.EXAMPLE
+  Create a production subscription and add the user to the owner role
+  .\New-AzureEaSubscription.ps1 -DestinationManagementGroupId "ntc-lz-sbx" -SubscriptionName "Name" -SubAlias "Name" -BillingScope "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321" -UserUPN "user@consto.com"
+.EXAMPLE
+  Create a production subscription and add the reader role to a user
+  .\New-AzureEaSubscription.ps1 -DestinationManagementGroupId "ntc-lz-sbx" -SubscriptionName "Name" -SubAlias "Name" -BillingScope "/providers/Microsoft.Billing/BillingAccounts/1234567/enrollmentAccounts/7654321" -UserUPN "user@consto.com" -RoleName "Reader"
 #>
 
 
@@ -43,7 +54,9 @@ Param (
   [Parameter(Mandatory=$true)][string]$SubscriptionName,
   [Parameter(Mandatory=$true)][string]$DestinationManagementGroupId,
   [Parameter(Mandatory=$true)][string]$BillingScope,
-  [Parameter(Mandatory=$false)][string]$WorkloadType = "Production"
+  [Parameter(Mandatory=$false)][string]$UserUPN,
+  [Parameter(Mandatory=$false)][string]$WorkloadType = "Production",
+  [Parameter(Mandatory=$false)][string]$RoleName = "Owner"
 )
 
 Connect-AzAccount 
@@ -74,3 +87,13 @@ Start-Sleep -s 20
 $NewSubscription = Get-AzSubscription -subscriptionname $SubscriptionName | Select-Object *
 New-AzManagementGroupSubscription -GroupId $DestinationManagementGroupId -SubscriptionId $NewSubscription.Id  
 Write-Host ("Move Sub: " + $NewSubscription.Id  + " to Management group: $DestinationManagementGroupId") -ForegroundColor Green
+
+## Will show an error if the role assignment already exisits
+if ($UserUPN) {
+    $Scope = "/subscriptions/" + $NewSubscription.Id 
+    $ObjID = (Get-AzADUser -UserPrincipalName $UserUPN).Id
+    # (Get-AzADGroup -DisplayName <groupName>).id
+    New-AzRoleAssignment -ObjectId $ObjID `
+            -RoleDefinitionName $RoleName `
+            -Scope $Scope
+}
